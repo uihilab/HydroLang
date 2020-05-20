e33ed57db84d0ab5cb44cc70be3b15eea68fe30a
Yusuf <muhammedyusuf-sermet@uiowa.edu>
2020-05-20 16:36:20 -0500

    Add static functions for +3rd level chaining

--- hydrolang/components/analyze/analyze.js
+++ hydrolang/components/analyze/analyze.js
@@ -1,24 +1,30 @@
-/**
- * Brief summary of what this function does
- * @param {example data type} example description
- * @returns {example data type} example description
- * @example
- * example usage code here
- */
-function stats(params) {
-	return "stats function is called";
+// Static functions are used after 3rd level of chaining.
+// Add only static functions which does not rely on data
+// stored on objects, i.e. only uses the data that is
+// given to the function as a parameter 
+
+
+class stats {
+
+    /** min: minimum value of an array
+     * @param {array} array with data.
+     * @returns {var} variable with min value of dataset.
+     */
+	static min(arr) {
+	    var low = arr[0];
+	    var i=0;
+	    while (++i <arr.length)
+	        if (arr[i] <low)
+	            low = arr[i];
+	    return low;
+	}
 }
 
-/**
- * Brief summary of what this function does
- * @param {example data type} example description
- * @returns {example data type} example description
- * @example
- * example usage code here
- */
-function hydro(params) {
-	return "hydro function is called";
+
+class hydro {
+
 }
+
 
 export{
 	stats,
