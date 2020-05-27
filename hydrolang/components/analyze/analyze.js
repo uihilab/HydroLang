b6148fd59b0f935a80dbbb092276022d9dd99d48
erazocar <erazcar8@aquacloud.net>
2020-05-27 23:47:43 +0200

    added thiessen and arithmetic to analyze & index

--- hydrolang/components/analyze/analyze.js
+++ hydrolang/components/analyze/analyze.js
@@ -232,36 +232,71 @@ class stats {
 
 class hydro {
 
-	/**Arithmetic mean: computation of aereal mean precipitation for a river basin given it has different stations.
-     * @param {data} object with time series data for different stations throughout the basin.
+    /**Total precipitation: arithmetic sum of the total amount of precipitation during an event.
+     * It is also used as a helper function.
+     * @param {arr} array with precipitation event.
+     * @returns {var} total amount of precipitation during an event on a given station. 
+     */
+
+    static totalprec (arr) {
+        var sum=0;
+        var k = arr.length
+        while (--k >=0) {
+            sum+=minarr[k]
+        };
+        return sum;
+    };
+
+    /**Arithmetic mean: computation of aereal mean precipitation for a river basin given it has 2 or more different stations.
+     * @param {array} object with precipitation with equal amounts of data from different rain gauges.
      * @returns {array} object with average precipitaiton for a specific time series.
      */
-    static arithmetic (data) {
-        var arr1 = [];
+
+    static arithmetic (arr) {
+        var average = [];
+        var final = [];
+        var n= arr.length;        
         for (var i=0; i < arr.length; i++){
-           arr1.push()             
-        }
-	 };
+            for (var j=0;j < arr[0].length;j++) {
+                average.push(arr[i][j])
+            };     
+        };
+        for (var h = 0; h<average.length;h+=n){
+            var minarr = average.slice(h,h+n);
+            final[h] = this.totalprec(minarr)/minarr.length;
+            var filtered = final.filter(function (el) {return el != null});
+        }; 
+        return filtered;
+    };
 	 
-     /**Thiessen polygon: calculates average precipitation for a basin considering there is
+    /**Thiessen polygon: calculates average precipitation for a basin considering there is
       * one station per sub basin.
-      * @param {data} object with arrays of time series.
-      * @param {data} object with arrays of areas per subbasin.
+      * @param {params} parameter object which has the time series data and area per subbasin.
       * @returns {array} time series of average precipitation over whole sub basin.
       */
-     static thiessen (arr1, arr2) {
-        var count = arr2.count();
-        outputarr = [];
-        for (var i = 0; i<arr1.length; i++){
-            outputarr[i]= arr1[i]*arr2[i]
-        }
+
+     static thiessen (params) {
+        var precs = params["rainfall"];
+        var areas = params["areas"];
+        var totarea = this.totalprec(areas);
+        var res = Array(precs.length).fill(0).map(()=>Array(areas.length).fill(0));
+        var out = Array(precs[0].length).fill(0);
+        for (var i = 0; i<precs.length; i++){
+            for(var j = 0; j < precs[0].length;j++) {
+                res[i][j]= precs[i][j] * areas[i];
+                out[j] += res[i][j] / totarea;
+            };
+        }; 
+        return out;
     };
 
     /** bucketmodel: does simple rainfall-runoff analyses over a rainfall dataset given landuse, baseflow and infiltration capacity.
      * @param {param} parameter object landuse, rainfall, infiltration capacity and baseflow.
      * @returns {array} values for runoff as time series.
      */
+
     static bucketmodel (params) {
+
         //initial parameters
         let rainfall = params["rainfall"];
         let n = rainfall.length;
@@ -275,6 +310,7 @@ class hydro {
         //infiltration capacities for agriculture, bare rock, grassland, forest and
         //moorland, respectively.
         let FieldCaps = [5,50,25,25,5];
+
         //arrays and variables
         var initial=Array(landuse.length).fill(0).map(()=>Array(n).fill(0));
         var interflow = Array(landuse.length).fill(0).map(()=>Array(n).fill(0));
@@ -320,10 +356,10 @@ class hydro {
             }
         };
         
-                //calculating the total amount of flow from overflow, baseflow and interflow
+        //calculating the total amount of flow from overflow, baseflow and interflow
         for (var j = 0; j < FieldCaps.length; j++) {
             for (var h = 0; h < n;h++) {
-                totalflow[j][h] = overflow[j][h]+interflow[j][h];
+                totalflow[j][h] = overflow[j][h]+interflow[j][h]+baseflow;
             }
         }
         //calculating total runoff
@@ -331,10 +367,10 @@ class hydro {
             totalrunoff[q] = totalflow[0][q] * landuse[0] + totalflow[1][q] * landuse[1]
             + totalflow[2][q] * landuse[2] + totalflow[3][q] * landuse[3]
             + totalflow[4][q] * landuse[4];
-        };
-        
+        }; 
         return totalrunoff;
-        };
+    };
+    
 }
 
 
