6b0446cf2734b3acf8f14d6915cd3069318d5dca
erazocar <erazcar8@aquacloud.net>
2022-07-27 14:59:14 -0500

    added CUAHSI data sources and SOAP envelope request header

--- hydrolang/modules/data/datasources.js
+++ hydrolang/modules/data/datasources.js
@@ -9,15 +9,23 @@
  * @extends aemet
  * @extends eauk
  * @extends meteoit
+ * @extends ifis
  */
 
-import usgs from "./datasources/usgs.js";
-import fema from "./datasources/fema.js";
-import noaa from "./datasources/noaa.js";
-import worldbank from "./datasources/worldbank.js";
-import meteostat from "./datasources/meteostat.js";
-import aemet from "./datasources/aemet.js";
-import eauk from "./datasources/eauk.js";
-import meteoit from "./datasources/meteoit.js";
-
-export { usgs, fema, noaa, worldbank, meteostat, aemet, eauk, meteoit };
+ import usgs from "./datasources/usgs.js";
+ import fema from "./datasources/fema.js";
+ import noaa from "./datasources/noaa.js";
+ import worldbank from "./datasources/worldbank.js";
+ import meteostat from "./datasources/meteostat.js";
+ import aemet from "./datasources/aemet.js";
+ import eauk from "./datasources/eauk.js";
+ import meteoit from "./datasources/meteoit.js";
+ import ifis from "./datasources/ifis.js";
+ import clearcreek from "./datasources/clearcreek.js"
+ import epa from "./datasources/epa.js"
+ import {waterOneFlow, hydroShare, hisCentral} from "./datasources/cuahsi.js"
+ import proxies from "./datasources/proxy.js"
+ import envelope from "./datasources/soapEnv.js";
+ 
+ export { usgs, fema, noaa, worldbank, meteostat, aemet, eauk, meteoit, ifis, clearcreek, epa, waterOneFlow, hydroShare, hisCentral, proxies, envelope};
+ 
---
+++ hydrolang/modules/data/datasources/soapEnv.js
@@ -0,0 +1,5 @@
+//Appends SOAP1.1 envelope to a request.
+const envelope = (body) => {
+  return `<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body>${body}</soap:Body></soap:Envelope>`;
+};
+export default envelope;
